064b3ec43448ffbe5f44fbcbe5a2cdd70c60a746
Vishal <vishykum@ymail.com>
2025-05-09 00:07:31 -0700

    Updated cors origin

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ require("dotenv").config();
 
 const app = express();
 app.use(cors({
-    origin: "http://localhost:5173",  // your Vite frontend dev server
+    origin: "https://vishykum.github.io/job-tracker-frontend/",  // your Vite frontend dev server
     credentials: true
 }));
 app.use(express.json());
